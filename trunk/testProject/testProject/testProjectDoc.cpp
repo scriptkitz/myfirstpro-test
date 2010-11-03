6a32029e65f46bac6c4ee3f5d5915a78f6c1eb97
scriptkitz <scriptkitz@da890e6b-1f8b-8dbb-282d-e1a1f9b2274c>
2010-11-03 04:47:48 +0000

    MFC APP

---
+++ trunk/testProject/testProject/testProjectDoc.cpp
@@ -0,0 +1,79 @@
+
+// testProjectDoc.cpp : CtestProjectDoc 类的实现
+//
+
+#include "stdafx.h"
+#include "testProject.h"
+
+#include "testProjectDoc.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CtestProjectDoc
+
+IMPLEMENT_DYNCREATE(CtestProjectDoc, CDocument)
+
+BEGIN_MESSAGE_MAP(CtestProjectDoc, CDocument)
+END_MESSAGE_MAP()
+
+
+// CtestProjectDoc 构造/析构
+
+CtestProjectDoc::CtestProjectDoc()
+{
+	// TODO: 在此添加一次性构造代码
+
+}
+
+CtestProjectDoc::~CtestProjectDoc()
+{
+}
+
+BOOL CtestProjectDoc::OnNewDocument()
+{
+	if (!CDocument::OnNewDocument())
+		return FALSE;
+
+	// TODO: 在此添加重新初始化代码
+	// (SDI 文档将重用该文档)
+
+	return TRUE;
+}
+
+
+
+
+// CtestProjectDoc 序列化
+
+void CtestProjectDoc::Serialize(CArchive& ar)
+{
+	if (ar.IsStoring())
+	{
+		// TODO: 在此添加存储代码
+	}
+	else
+	{
+		// TODO: 在此添加加载代码
+	}
+}
+
+
+// CtestProjectDoc 诊断
+
+#ifdef _DEBUG
+void CtestProjectDoc::AssertValid() const
+{
+	CDocument::AssertValid();
+}
+
+void CtestProjectDoc::Dump(CDumpContext& dc) const
+{
+	CDocument::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CtestProjectDoc 命令
