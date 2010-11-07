d23ac6623deafdcd859c474df0abafe2044dd0a4
scriptkitz <scriptkitz@da890e6b-1f8b-8dbb-282d-e1a1f9b2274c>
2010-11-07 04:44:01 +0000

    进程列表对话框，添加刷新功能

--- trunk/testProject/testProject/Resource.h
+++ trunk/testProject/testProject/Resource.h
@@ -10,8 +10,9 @@
 #define ID_WINDOW_MANAGER               131
 #define IDS_WINDOWS_MANAGER             132
 #define IDD_PROCESS_LIST                310
-#define IDC_LIST1                       1008
 #define IDC_LIST_PROCESS                1008
+#define IDC_REFRASH                     1009
+#define IDC_REFRESH                     1009
 
 // Next default values for new objects
 // 
@@ -19,7 +20,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        313
 #define _APS_NEXT_COMMAND_VALUE         32771
-#define _APS_NEXT_CONTROL_VALUE         1009
+#define _APS_NEXT_CONTROL_VALUE         1010
 #define _APS_NEXT_SYMED_VALUE           310
 #endif
 #endif
