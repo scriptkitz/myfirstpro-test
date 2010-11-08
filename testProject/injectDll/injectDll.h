ae312d0c728e31cf2eb09f884974259fb9a98e5f
scriptkitz <scriptkitz@da890e6b-1f8b-8dbb-282d-e1a1f9b2274c>
2010-11-08 04:04:07 +0000

    添加新工程injectDll

---
+++ testProject/injectDll/injectDll.h
@@ -0,0 +1,22 @@
+// The following ifdef block is the standard way of creating macros which make exporting 
+// from a DLL simpler. All files within this DLL are compiled with the INJECTDLL_EXPORTS
+// symbol defined on the command line. this symbol should not be defined on any project
+// that uses this DLL. This way any other project whose source files include this file see 
+// INJECTDLL_API functions as being imported from a DLL, whereas this DLL sees symbols
+// defined with this macro as being exported.
+#ifdef INJECTDLL_EXPORTS
+#define INJECTDLL_API __declspec(dllexport)
+#else
+#define INJECTDLL_API __declspec(dllimport)
+#endif
+
+// This class is exported from the injectDll.dll
+class INJECTDLL_API CinjectDll {
+public:
+	CinjectDll(void);
+	// TODO: add your methods here.
+};
+
+extern INJECTDLL_API int ninjectDll;
+
+INJECTDLL_API int fninjectDll(void);
