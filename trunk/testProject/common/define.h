42a97e23d86e7fd988831ad761b4adbd31b62f8a
scriptkitz <scriptkitz@da890e6b-1f8b-8dbb-282d-e1a1f9b2274c>
2010-11-10 10:07:44 +0000

    实现关闭文档卸载钩子和模块功能。

--- trunk/testProject/common/define.h
+++ trunk/testProject/common/define.h
@@ -1,2 +1,45 @@
+
+
+
+#include <strsafe.h>
+#include "stdafx.h"
+#pragma once
+
+#ifndef DEFINE_FILE_ONCE
+#define DEFINE_FILE_ONCE
 #define  INJECT_DLL_NAME "injectDLL.dll"
 #define MYSTRING "z_j_2010_dll_scriptkit"
+
+inline void ErrorExit(LPTSTR lpszFunction) //= =，图个方便，写这里，但多个文件引用，引起重复定义编译错误，= =，懒的分开放了，直接写为inline，内联函数，谁调用谁新增代码= =，
+{ 
+	// Retrieve the system error message for the last-error code
+
+	LPVOID lpMsgBuf;
+	LPVOID lpDisplayBuf;
+	DWORD dw = GetLastError(); 
+
+	FormatMessage(
+		FORMAT_MESSAGE_ALLOCATE_BUFFER | 
+		FORMAT_MESSAGE_FROM_SYSTEM |
+		FORMAT_MESSAGE_IGNORE_INSERTS,
+		NULL,
+		dw,
+		MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT),
+		(LPTSTR) &lpMsgBuf,
+		0, NULL );
+
+	// Display the error message and exit the process
+
+	lpDisplayBuf = (LPVOID)LocalAlloc(LMEM_ZEROINIT, 
+		(lstrlen((LPCTSTR)lpMsgBuf) + lstrlen((LPCTSTR)lpszFunction) + 40) * sizeof(TCHAR)); 
+	StringCchPrintf((LPTSTR)lpDisplayBuf, 
+		LocalSize(lpDisplayBuf) / sizeof(TCHAR),
+		TEXT("%s failed with error %d: %s"), 
+		lpszFunction, dw, lpMsgBuf); 
+	MessageBox(NULL, (LPCTSTR)lpDisplayBuf, TEXT("Error"), MB_OK); 
+
+	LocalFree(lpMsgBuf);
+	LocalFree(lpDisplayBuf);
+	//ExitProcess(dw); 
+}
+#endif
