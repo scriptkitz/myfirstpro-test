cd74031066fdc79efa5083d423a8d223e0e0bd79
scriptkitz <scriptkitz@da890e6b-1f8b-8dbb-282d-e1a1f9b2274c>
2010-11-16 09:46:13 +0000

    勉强注入多个关闭偶尔会报错。需要继续改进。。疯了快

--- trunk/testProject/common/define.h
+++ trunk/testProject/common/define.h
@@ -10,22 +10,20 @@
 #define M_SIZE_SEMA 50 //信号量字符串长度;
 #define  MAPPINGFILESIZE 1024*100 //createfilemapping大小;
 #define  INJECT_DLL_NAME "injectDLL.dll" //dll文件名；
-#define READ_SEMAPHORE   "z_j_2010_dll_scriptkit_read"  //读信号量；
-#define WRITE_SEMAPHORE  "z_j_2010_dll_scriptkit_write" //写信号量；
+#define DLL_SEMAPHORE	"zj_dll_scriptkit_sema_" //DLL socket用信号量，领号排队，讲文明啊哈
+#define READ_SEMAPHORE   "z_j_2010_comm_scriptkit_read"  //读信号量；
+#define WRITE_SEMAPHORE  "z_j_2010_comm_scriptkit_write" //写信号量；
 #define MUTEX_STRING "Z_J_exe_2010_mutex_kk"	//互斥量
 #define FILE_MAPPING_STRING "zj_file_mapping_"
 struct dataHeader
 {
-	bool tofrom;//true to exe,false to dll
 	char sockType;//链接类型..char szSockType[6][6] = { "NUL", "TCP", "UDP", "RAW", "RDM", "SEQ" };
+	int cmd;	//命令。。exe对dll发送的命令。 0表示退出命令，要退出了。1表示不处理，直接通过，2表示拦截这次发送或者接收。3表示修改，修改这次的数据内容。
 	int method; //数据功能.1:send 2:recv 3:sendto 4:recvfrom 
 	size_t size;//数据大小
-	//DWORD threadID;//
-	//int index;//数据总索引
-	//int offset;//数据偏移指针地址.
-	struct sockaddr_in  localsock;
-	struct sockaddr_in  remotesock;
-	
+	int index;//
+	struct sockaddr_in  localsock; //目标ip，端口
+	struct sockaddr_in  remotesock;//目标的连接的ip，端口
 };
 
 inline void ErrorExit(LPTSTR lpszFunction) //= =，图个方便，写这里，但多个文件引用，引起重复定义编译错误，= =，懒的分开放了，直接写为inline，内联函数，谁调用谁新增代码= =，
